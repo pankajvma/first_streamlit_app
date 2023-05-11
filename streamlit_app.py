3aa05a50b01db2cfa274e1e941c912f2be5d0ebf
Pankaj Verma <47264501+pankajvma@users.noreply.github.com>
2023-05-11 13:45:33 +0530

    Update streamlit_app.py
    Added header for the new section.
    added functionality for receiving input from users to show the API response results

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,7 +22,12 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 # Display the table on the page.
 streamlit.dataframe(fruits_to_show)
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+# Adding heading for new section
+streamlit.header('Fruityvice Fruit Advice')
+fruit_choice = streamlit.text_input('What fruit would you like information about?')
+streamlit.write('The users entered', fruit_choice)
+
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit" + fruit_choice)
 # streamlit.text(fruityvice_response.json()) # just writes the json data to the screen
 
 # take the json verson of the response and normalize it
