360316f22a943bda7bd9edd5391e607a781ac1e2
Pankaj Verma <47264501+pankajvma@users.noreply.github.com>
2023-05-11 17:41:19 +0530

    Update streamlit_app.py
    Adding new fruit to Snowflake table with user input

--- streamlit_app.py
+++ streamlit_app.py
@@ -45,3 +45,7 @@ my_cur.execute('SELECT * FROM fruit_load_list')
 my_data_row = my_cur.fetchall()
 streamlit.text("The fruit_load_list contains:")
 streamlit.dataframe(my_data_row)
+
+add_fruit = streamlit.text_input('What fruit would you like to add?', 'Kiwi')
+my_cur.execute(f'INSERT INTO fruit_load_list VALUES ({add_fruit})')
+streamlit.write('Thanks for adding', add_fruit)
